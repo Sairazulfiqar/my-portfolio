2840585e82a52ff2ec17cc43aa40f071890eb579
Sairazulfiqar <sairazulfiqaranjum@gmail.com>
2023-08-19 02:01:57 +0200

    portfolio

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import "../App.css";
+// import {useForm} from 'react-hook-form';
 
-export const Contact = () =>{
+export const Contact = () => {
   return (
     <div className="flex-child5" id="Contact">
-      Contact
+      <form name="formulario" onsubmit="return validation()" action="confirmation.php"
+        method="post">
+        <div class="row">
+          <label for="nombre">Nombre:</label>
+          <input type="text" onkeyup="validarNombre()" id="nombre" class="nombre" name="nombre"/>
+            <div class="error" id="errorNombre"></div>
+        </div>
+        <div class="row">
+          <label for="email">Email:</label>
+          <input type="email" onkeyup="validarEmail()" id="email" class="email" name="email"/>
+            <div class="error" id="erroremail"></div>
+        </div>
+      </form>
+      <div>
+        <input type="submit" value="Enviar"/>
+      </div>
     </div>
+
   )
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -10,27 +10,25 @@ export const Home = () => {
   const handleDone = () => {
   }
 
-  return (
-    <div className="flex-child1" id="Home"> 
-    {/* <img src={girl} alt='' id='girl'/> */} 
-      <h1 style={{ paddingTop: '70px', margin: '200px', fontSize:'50px', fontWeight: 'normal' }}>
-        I am a{' '}
-        <span style={{ color: 'purple', fontWeight: 'bold' }}>
-          {/* Style will be inherited from the parent element */}
-          <Typewriter
-            words={['Web Developer', 'Political Scientist', 'Freelancer']}
-            loop={3}
-            cursor
-            cursorStyle= '_'
-            typeSpeed={70} //Character typing speed in Milliseconds
-            deleteSpeed={50} //Character deleting speed in Milliseconds
-            delaySpeed={1000}
-            onLoopDone={handleDone}
-
-          />
-        </span>
-      </h1>
-    </div>
+return (
+  <div className="flex-child1" id="Home"> 
+  {/* <img src={girl} alt='' id='girl'/> */} 
+    <h1 style={{ paddingTop: '70px', margin: '200px', fontSize:'50px', fontWeight: 'normal' }}>
+      I am a{' '}
+      <span style={{ color: 'purple', fontWeight: 'bold' }}>
+        {/* Style will be inherited from the parent element */}
+        <Typewriter
+          words={['Web Developer', 'Political Scientist', 'Freelancer']}
+          loop={3}
+          cursor
+          cursorStyle= '|'
+          typeSpeed={70} //Character typing speed in Milliseconds
+          deleteSpeed={50} //Character deleting speed in Milliseconds            delaySpeed={1000}
+          onLoopDone={handleDone}
+        />
+      </span>
+    </h1>
+  </div>
   )
 };
 
